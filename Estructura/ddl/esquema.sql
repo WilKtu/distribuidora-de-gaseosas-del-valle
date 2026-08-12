a1f6501aff0921826997a01e57896383c14e5aa3
WilKtu <fernandocolaj7@gmail.com>
2026-08-11 20:02:40 -0600

    feat(auth): otorgar permisos de acceso al usuario desarrollador

--- Estructura/ddl/esquema.sql
+++ Estructura/ddl/esquema.sql
@@ -1,3 +1,15 @@
 -- Crear el usuario
 CREATE USER 'desarrollador'@'localhost'
 IDENTIFIED BY 'Desarrollador123*';
+
+-- Dar permisos sobre la base de datos
+GRANT
+    SELECT, INSERT,
+    UPDATE, DELETE,
+    CREATE, ALTER,
+    INDEX, CREATE VIEW,
+    SHOW VIEW, CREATE ROUTINE,
+    ALTER ROUTINE, EXECUTE,
+    TRIGGER, EVENT
+ON gaseosas_del_valle*
+TO 'desarrollador'@'localhost';
