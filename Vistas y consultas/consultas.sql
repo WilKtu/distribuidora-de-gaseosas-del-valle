d5b3c574f97181c6d49d75d65cb9f184f7cd6c56
WilKtu <fernandocolaj7@gmail.com>
2026-08-12 20:18:06 -0600

    feat(consultas): agregar consulta de productos más vendidos

--- Vistas y consultas/consultas.sql
+++ Vistas y consultas/consultas.sql
@@ -7,12 +7,21 @@ USE gaseosas_del_valle;
 
 -- Consulta 1: Consultar los productos con stock por debajo del mínimo
 SELECT nombre, stock_actual, stock_minimo 
-FROM productos 
-WHERE stock_actual < stock_minimo;
+    FROM productos 
+    WHERE stock_actual < stock_minimo;
 
 -- ======================================================
 
 -- Consulta 2: Consultar los pedidos realizados entre dos fechas (BETWEEN)
 SELECT id_pedido, fecha_pedido, total_con_iva 
-FROM pedidos 
-WHERE fecha_pedido BETWEEN '2026-01-01' AND '2026-03-31'; 
+    FROM pedidos 
+    WHERE fecha_pedido BETWEEN '2026-01-01' AND '2026-03-31';
+
+-- ======================================================
+
+-- Consulta 3: Listar los productos más vendidos (con JOIN y GROUP BY)
+SELECT p.nombre, SUM(dp.cantidad) AS total_vendido
+    FROM productos p
+    JOIN detalle_pedido dp ON p.id_producto = dp.id_producto
+    GROUP BY p.nombre
+    ORDER BY total_vendido DESC;
