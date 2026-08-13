d1644c77ae78212ba58d8599327c3759d12a959e
Wilktu <fernandocolaj7@gmail.com>
2026-08-13 14:27:05 -0600

    feat(trigger): crear trigger tr_auditar_cambio_precio

--- Triggers/triggers.sql
+++ Triggers/triggers.sql
@@ -17,3 +17,37 @@ BEGIN
 END //
 
 DELIMITER ;
+
+-- Trigger 2: Registrar cambios de precio
+
+-- ============================================================
+
+DELIMITER //
+
+CREATE TRIGGER tr_auditar_cambio_precio
+AFTER UPDATE ON productos
+FOR EACH ROW
+BEGIN
+
+    IF OLD.precio <> NEW.precio THEN
+
+        INSERT INTO auditoria_precios
+        (
+            id_producto,
+            precio_anterior,
+            precio_nuevo,
+            fecha_cambio
+        )
+        VALUES
+        (
+            NEW.id_producto,
+            OLD.precio,
+            NEW.precio,
+            NOW()
+        );
+
+    END IF;
+
+END //
+
+DELIMITER ;
