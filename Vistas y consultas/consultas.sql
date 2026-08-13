2ee79b884bdca6529475c4fa66914c11a12f4485
WilKtu <fernandocolaj7@gmail.com>
2026-08-12 20:22:36 -0600

    feat(consultas): agregar consulta de búsqueda de clientes por nombre

--- Vistas y consultas/consultas.sql
+++ Vistas y consultas/consultas.sql
@@ -33,3 +33,10 @@ SELECT c.nombre, COUNT(p.id_pedido) AS cantidad_pedidos
     FROM clientes c
     LEFT JOIN pedidos p ON c.id_cliente = p.id_cliente
     GROUP BY c.id_cliente, c.nombre;
+
+-- ======================================================
+
+-- Consulta 5: Buscar clientes por nombre parcial usando LIKE
+SELECT nombre, correo 
+    FROM clientes 
+    WHERE nombre LIKE '%Supermercado%';
